4ab9ed307d0b60c9cd20288768c2fa1f9ab1e766
murilolemes <murilo.lemes.mhl@gmail.com>
2020-07-22 22:30:26 -0300

    finalizando primeiro projeto com node.js

--- nivel-2/Primeiro-Projeto-Node/src/routes/users.routes.ts
+++ nivel-2/Primeiro-Projeto-Node/src/routes/users.routes.ts
@@ -2,30 +2,28 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import ensureAuthenticaded from '../middleware/ensureAuthenticated';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return res.json(user);
-  } catch (err) {
-    return res.status(400).json({ error: err.message });
-  }
+  return res.json(user);
 });
 
 usersRouter.patch(
@@ -33,8 +31,16 @@ usersRouter.patch(
   ensureAuthenticaded,
   upload.single('avatar'),
   async (req, res) => {
-    console.log(req.file);
-    return res.json({ ok: true });
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: req.user.id,
+      avatarFilename: req.file.filename,
+    });
+
+    delete user.password;
+
+    return res.json(user);
   },
 );
 
